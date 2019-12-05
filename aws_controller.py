d7d18ec3acecb997f5d768421c39d771bb436f9b
bs <seemannb@hs-pforzheim.de>
2019-12-05 11:12:23 +0100

    fix no table object 01

--- aws_controller.py
+++ aws_controller.py
@@ -1,14 +1,14 @@
 import boto3
 
 dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
-table = dynamo_client.Table('YourTestTable')
+# table = dynamo_client.Table('YourTestTable')
 
-response = table.put_item(
-    Item={
-        'Artist': 'Thomas Schuster',
-        'Song': 'Professor'
-    }
-)
+# response = table.put_item(
+#     Item={
+#         'Artist': 'Thomas Schuster',
+#         'Song': 'Professor'
+#     }
+# )
 
 db = boto3.resource('dynamodb', region_name='us-east-1')
 table = db.Table('YourTestTable')
