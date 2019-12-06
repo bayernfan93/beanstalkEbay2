55a7c65b7f6a9b94f8c8af1dc682709387e66353
bs <seemannb@hs-pforzheim.de>
2019-12-06 14:50:27 +0100

    put item

--- aws_controller.py
+++ aws_controller.py
@@ -3,15 +3,15 @@ import boto3
 dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
 # table = dynamo_client.Table('YourTestTable')
 
-# response = table.put_item(
-#     Item={
-#         'Artist': 'Thomas Schuster',
-#         'Song': 'Professor'
-#     }
-# )
-
 db = boto3.resource('dynamodb', region_name='us-east-1')
 table = db.Table('YourTestTable')
+
+response = table.put_item(
+    Item={
+        'Artist': 'Marcel Viehmaier',
+        'Song': 'Student'
+    }
+)
 
 
 def get_items():
