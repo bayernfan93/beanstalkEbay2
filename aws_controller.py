fdda390980ad3ec5a66f4fd2aa9e73ce951cc432
bs <seemannb@hs-pforzheim.de>
2019-12-06 15:04:55 +0100

    put item if else vorhandene Daten überprüfen

--- aws_controller.py
+++ aws_controller.py
@@ -6,12 +6,21 @@ dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
 db = boto3.resource('dynamodb', region_name='us-east-1')
 table = db.Table('YourTestTable')
 
-response = table.put_item(
-    Item={
-        'Artist': 'Marcel Viehmaier',
-        'Song': 'Student'
-    }
-)
+allItems = dynamo_client.scan(TableName='YourTestTable')
+
+for key, value in allItems:
+    print
+    key, value
+
+if key != 'Marcel Viehmaier':
+    response = table.put_item(
+        Item={
+            'Artist': 'Marcel Viehmaier',
+            'Song': 'Student'
+        }
+    )
+else:
+    print("WTF")
 
 
 def get_items():
