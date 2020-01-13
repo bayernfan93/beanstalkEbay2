d2d48bcc71ed967138d7d835b7142ae865884711
Benjamin Seemann <seemannb@hs-pforzheim.de>
2020-01-13 19:30:47 +0100

    13012019-02

--- aws_controller.py
+++ aws_controller.py
@@ -1,24 +1,24 @@
-import boto3
-
-dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
-# table = dynamo_client.Table('YourTestTable')
-
-db = boto3.resource('dynamodb', region_name='us-east-1')
-table = db.Table('YourTestTable')
-
-allItems = dynamo_client.scan(TableName='YourTestTable')
-
-for key, value in allItems:
-    if key != 'Marcel Viehmaier':
-        response = table.put_item(
-            Item={
-                'Artist': 'Marcel Viehmaier',
-                'Song': 'Student'
-            }
-        )
-    else:
-        print("WTF")
-
-
-def get_items():
-    return dynamo_client.scan(TableName='YourTestTable')
+# import boto3
+#
+# dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
+# # table = dynamo_client.Table('YourTestTable')
+#
+# db = boto3.resource('dynamodb', region_name='us-east-1')
+# table = db.Table('YourTestTable')
+#
+# allItems = dynamo_client.scan(TableName='YourTestTable')
+#
+# for key, value in allItems:
+#     if key != 'Marcel Viehmaier':
+#         response = table.put_item(
+#             Item={
+#                 'Artist': 'Marcel Viehmaier',
+#                 'Song': 'Student'
+#             }
+#         )
+#     else:
+#         print("WTF")
+#
+#
+# def get_items():
+#     return dynamo_client.scan(TableName='YourTestTable')
