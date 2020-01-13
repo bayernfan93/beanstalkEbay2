a2859feca26f774070cd239ecf4fec397ba5afe4
Benjamin Seemann <seemannb@hs-pforzheim.de>
2020-01-13 19:26:14 +0100

    13012019-01

--- application.py
+++ application.py
@@ -9,13 +9,13 @@ application = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-# @application.route('/get-items')
-# def get_items():
-#     return render_template('get-items.html')
-
 @application.route('/get-items')
 def get_items():
-    return jsonify(aws_controller.get_items())
+    return render_template('get-items.html')
+
+# @application.route('/get-items')
+# def get_items():
+#     return jsonify(aws_controller.get_items())
 
 if __name__ == '__main__':
     application.run()
